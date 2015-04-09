fc80ae023fea30f85166e89d7318b26700d1d1bb
Florian Weber <florian.weber@ueberbit.de>
2015-04-09 20:02:17 +0200

    Remove drush autloader override, require drupal and drush dev

--- web/drush/drushrc.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Make Drush use the project's autoloader instead of Drupal core's hardcoded
-// one.
-$autoloader = require __DIR__ . '/../../vendor/autoload.php';
-drush_drupal_load_autoloader(NULL, $autoloader);
